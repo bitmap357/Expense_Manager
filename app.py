499b621bc5f79cece03444e0806012819c4fe8c7
Edmund Akogyeram <edakoyeram@gmail.com>
2023-12-29 20:58:45 +0000

    db config

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String(50), nullable=False)
     expensename = db.Column(db.String(50), nullable=False)
-    amount = db.Column(db.Integer(50), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
     category = db.Column(db.String(50), nullable=False)
 
     
