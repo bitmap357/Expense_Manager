ce61f16a3bbbc61a4fbbe3d86119c19987fe1896
bitmap357 <edakoyeram@gmail.com>
2024-02-07 22:49:28 +0000

    id

--- .history/app_20240207224918.py
+++ .history/app_20240207224918.py
@@ -26,8 +26,6 @@ login_manager.login_view = 'login'
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
